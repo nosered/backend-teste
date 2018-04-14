5b765d04fd59a25efa3d4be858a181c350591cc5
Ederson Abreu <ederson.dimm@gmail.com>
2018-04-14 20:50:42 -0300

    heroku config

--- models/index.js
+++ models/index.js
@@ -4,14 +4,34 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+//var env       = process.env.NODE_ENV || 'development';
+//var config    = require(__dirname + '/../config/config.json')[env];
 
-if (config.use_env_variable) {
+const heroku = {
+  host: process.env.HEROKU_HOST,
+  database: process.env.HEROKU_DATABASE,
+  username: process.env.HEROKU_USERNAME,
+  password: process.env.HEROKU_PASSWORD
+};
+
+var db = {};
+
+/*if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}*/
+
+if(heroku.host && heroku.database && heroku.username && heroku.password) {
+  var sequelize = new Sequelize(heroku.database, heroku.username, heroku.password, {
+    host: heroku.host,
+    dialect: "postgres"
+  });
+} else {
+  var sequelize = new Sequelize('apidb', 'postgres', 'postgres', {
+    host: "127.0.0.1",
+    dialect: "postgres"
+  });
 }
 
 fs
